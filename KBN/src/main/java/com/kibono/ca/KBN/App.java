e7731e39ace054fd04737002dda9a47cfadde347
sandeepakki <sandeep.akki@assystant.com>
2022-02-19 15:16:42 +0530

    Kibono Onboarding

---
+++ KBN/src/main/java/com/kibono/ca/KBN/App.java
@@ -0,0 +1,13 @@
+package com.kibono.ca.KBN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
